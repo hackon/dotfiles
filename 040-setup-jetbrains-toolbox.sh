1b7df831e06731baa20fbde1df9114f61f4df8a4
Haakon Heiberg <haakon.heiberg@gmail.com>
2017-09-24 14:38:23 +0200

    add toolbox to bin and create zprofile for zsh

--- 040-setup-jetbrains-toolbox.sh
+++ 040-setup-jetbrains-toolbox.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.4.2492.tar.gz
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.4.2492.tar.gz
 
-#tar zxvf jetbrains-toolbox-1.4.2492.tar.gz
+tar zxvf jetbrains-toolbox-1.4.2492.tar.gz
 
 cd jetbrains-toolbox-1.4.2492
 
 ./jetbrains-toolbox 
 
-unlink $HOME/.bin/toolbox
+unlink $HOME/bin/toolbox
 
-ln -s $HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox $HOME/.bin/toolbox
+ln -s $HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox $HOME/bin/toolbox
 
 cd ..
 rm jetbrains-toolbox-1.4.2492.tar.gz
+
+cp -r $HOME/.profile $HOME/.zprofile
